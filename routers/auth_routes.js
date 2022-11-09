4793e8ab11f40cf08783b2454985ccc2c119408a
Suryan_ <suryan0415@gmail.com>
2022-11-09 16:05:40 +0530

    auth add

--- routers/auth_routes.js
+++ routers/auth_routes.js
@@ -1,19 +1,19 @@
 const { expressjwt:jwt }=require("express-jwt")
 
 exports.requireSignIn = jwt({
-    secret:process.env.SECRET_KEY.at,
+    secret:process.env.SECRET_KEY,
     algorithms:["HS256"],
     userProperty:"auth"
 })
 
 exports.isAuth = (req, res, next) => {
-    console.log("Auth: ", req.auth)
-    // let user = req. && req. && req.profile._id == req.auth._id;
+    console.log(req.auth)
     if (!user) {
         return res.status(403).json({
             err: "Access denied"
         });
     }
+    
     next();
 };
 
